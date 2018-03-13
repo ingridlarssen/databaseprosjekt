bcea2d22f43a1879c41ce8aa5339c5512ac15dd3
eivz0r <eivind.rydningen@gmail.com>
2018-03-13 13:16:49 +0100

    Implemented connection establisher

---
+++ DatabaseProsjekt/src/program/ConnectionEstablisher.java
@@ -0,0 +1,38 @@
+package program;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionEstablisher {
+	
+	private static String username = "eivinry_DB";
+	private static String pw = "gruppe100";
+	private static String dbName = "jdbc:mysql://mysql.stud.ntnu.no/eivinry_prosjekt_gr100";
+	public java.sql.Connection myConnection;
+	static int connectionCounter;
+	
+	public ConnectionEstablisher(){
+		this.establishConnection();
+	}
+		
+	private void establishConnection() {
+		try {
+			java.sql.Connection kobling = DriverManager.getConnection(dbName ,username, pw);
+			this.myConnection = kobling;
+			connectionCounter++;
+			System.out.println("Established a new connection with connection number: " + connectionCounter);
+			
+		} catch (SQLException e) {
+			System.out.println("Could not connect due to: " + "\n" + e.getStackTrace());
+		}
+	}
+	
+	public void closeConnection() {
+		try {
+			this.myConnection.close();
+		} catch (SQLException e) {
+			System.out.println("Failed to close connection due to: \n"+ e.getStackTrace());
+		}
+		connectionCounter--;
+	}	
+}
