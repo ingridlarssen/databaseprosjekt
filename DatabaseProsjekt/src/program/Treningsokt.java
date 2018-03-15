85097ad25e37c4616c59b73b0ba9ed181096b3fa
ingridlarssen <ingrid_al_96@hotmail.com>
2018-03-15 13:51:00 +0100

    klasse for treningsøkt

--- DatabaseProsjekt/src/program/Treningsokt.java
+++ DatabaseProsjekt/src/program/Treningsokt.java
@@ -1,25 +1,41 @@
 package program;
 
+import java.sql.SQLException;
+
 public class Treningsokt {
 	
 	private int dato;
 	private int tidspunkt;
-	private int varighetMinutter;
+	private int varighetMin;
 	private int form;
 	private int prestasjon;
 	private String beskrivelse;
 	
-	public Treningsokt(int dato, int tidspunkt, int varighetMinutter, int form, int prestasjon, String beskrivelse) {
+	public Treningsokt(int dato, int tidspunkt, int varighetMin, int form, int prestasjon, String beskrivelse) {
 		this.dato = dato;
 		this.tidspunkt = tidspunkt;
-		this.varighetMinutter = varighetMinutter;
+		this.varighetMin = varighetMin;
 		this.form = form;
 		this.prestasjon = prestasjon;
 		this.beskrivelse = beskrivelse;
 	}
 	
-	public void leggTilTreningsokt() {
-		//legg til treningsøkt i databasen
+	public void leggTilTreningsokt(ConnectionEstablisher connection, Treningsokt okt) {
+		try {
+			java.sql.PreparedStatement prep = connection.myConnection.prepareStatement(""
+					+ "INSERT INTO Treningsokt(okt.dato, okt.tidspunk, okt.varighetMin, bla)");
+			prep.setString(1, okt.getDato());
+			prep.setString(2, okt.getTidspunkt());
+			prep.setLong(3, dep.getTlf());
+			prep.setLong(4, dep.getBrukerTlf());
+			prep.setLong(5, dep.isPrimary());
+			prep.setLong(6, dep.isActive());
+			prep.setString(7, dep.getRelationType());
+			prep.setLong(8, dep.getNotificationType());
+			prep.executeUpdate();
+		} catch (SQLException e){
+			System.out.println("Kunne ikke legge til treningsøkt");
+		}
 	}
 
 	public int getDato() {
