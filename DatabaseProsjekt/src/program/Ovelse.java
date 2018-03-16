32fda839dacb7ecd00c7242592ab3915650934c8
eivz0r <eivind.rydningen@gmail.com>
2018-03-16 11:46:54 +0100

    Implemented getMaxOvelseID in the Ovelse class

--- DatabaseProsjekt/src/program/Ovelse.java
+++ DatabaseProsjekt/src/program/Ovelse.java
@@ -1,5 +1,8 @@
 package program;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Ovelse {
 	
 	private String navn;
@@ -16,7 +19,21 @@ public class Ovelse {
 		this.navn = navn;
 	}
 	
-	
+	public static int getMaxOvelseID(ConnectionEstablisher connection) throws SQLException {
+		String sql = "SELECT OvelseID, MAX(OvelseID) FROM Ovelse GROUP BY OvelseID";
+		try {
+			int maxID = 0;
+			java.sql.Statement st = connection.myConnection.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next()) {
+				maxID = rs.getInt(1);	
+			}		
+			return maxID + 1;
+		} catch(SQLException m) {
+			m.printStackTrace();
+			throw new SQLException("Fillern!");
+		} 
+	}
 	
 
 }
